8968426d85f8eefbae238b0ac046742c1dd466da
iamlukovkin <ivan@iamlukovkin.ru>
2023-10-29 14:33:18 +0300

    init

---
+++ EVM'n'PU-progs/main.c
@@ -0,0 +1,18 @@
+/*
+ * EVM'n'PU-progs.c
+ *
+ * Created: 29.10.2023 14:32:20
+ * Author : Ivan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
